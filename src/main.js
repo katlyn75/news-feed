259e27685b2dc5e9aba7654b54221c2aca2e0fb1
Katlyn Tucker <studioquirk@yahoo.com>
2018-03-27 15:33:25 -0700

    modified html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './reset.css';
 import './main.css';
+import Article from './components/articles/Article';
+import ArticleList from './components/articles/ArticleList';
 import App from './components/app/App';
 
 const root = document.getElementById('root');
--- src/services/newssApi.js
+++ src/services/newssApi.js
@@ -1,12 +1,12 @@
 
 const KEY = 'fb4c4673400a4afbb04a5b318aead7fc';
-const NEWS_URL = `https://newsapi.org/v2/top-headlines?sources=wired&apiKey=${KEY}`;
+const NEWS_URL = `https://newsapi.org/v2/top-headlines?sources?&apiKey=${KEY}`;
 
 const storage = window.localStorage;
 
 export function searchNews(searchTerm) {
-  const url = `${NEWS_URL}&q=${searchTerm}&maxResults=20`;
-  //show top 20 results
+  const url = `${NEWS_URL}&q=${searchTerm}&maxResults=25`;
+  //show top 25 results
 
   const data = storage.getItem(url);
   if(data) return Promise.resolve(JSON.parse(data));
